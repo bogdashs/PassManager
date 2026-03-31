73b5c47ec5090b04e0ac157889979f0ca8ab63d0
Admin <kolavlasuk848@gmail.com>
2026-03-31 12:13:04 +0300

    PassManager 0.1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+
+std::string input(std::string promt) {
+    std::string value;
+    std::cout << promt;
+    std::getline(std::cin, value);
+    return value;
+
+}
+void print(std::string promt) {
+    std::cout << promt;
+}
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    std::string pass = input("Ваш пароль:");
+    print("Вы ввели: " + pass);
+
+    return 0;
+}
