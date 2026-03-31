bbb00126f6fdcc454f8c9f9a075f898e865dd47f
Admin <kolavlasuk848@gmail.com>
2026-03-31 13:33:29 +0300

    PassManager 0.3

--- main.cpp
+++ main.cpp
@@ -9,23 +9,65 @@ std::string input(std::string promt) {
 
 }
 void print(std::string promt) {
-    std::cout << promt;
+    std::cout << promt << std::endl;
+}
+
+void showmenu() {
+    print("[+] ==== PASS MANAGER ====");
+    print("[+] 1.Записать новый пароль");
+    print("[+] 2.Изменить пароль");
+    print("[+] 3.Посмотреть пароль");
+    print("[+] 4.Выход");
+}
+
+std::string enc(std::string data,char key) {
+
+    std::string output = data;
+
+    for (int i = 0; i < output.size(); i++) {
+        output[i] = data[i] ^ key;
+    }
+
+    return output;
+}
+
+void add_to_db(std::string name,std::string pass) {
+    std::ofstream file("database.txt",std::ios::app);
+
 }
 
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
     std::string pass;
+    std::string name;
+    std::string choice;
     while (true) {
-        pass = input("Ваш пароль:");
-        // print("Вы ввели: " + pass);
-        if (pass.empty()) {
-            print("Вы ничего не ввели!\n");
-            continue;
+
+        showmenu();
+
+        choice = input("Выберите действие: ");
+
+        if (choice == "1") {
+            name = input("Название соцсети: ");
+            pass = input("Ваш пароль: ");
+
+            if (pass.empty() || name.empty()) {
+                print("Вы ничего не ввели!");
+                continue;
+            }
         }
 
+
+        print("Успешно сохранено! ");
         break;
     }
-    print("Вы ввели: " + pass);
+    // print("Вы ввели: " + name);
+    // print("Вы ввели: " + pass);
+    // std::string ENCpass = enc(pass,35);
+    // print("Enc: " + ENCpass);
+    // std::string DECpass = enc(ENCpass,35);
+    // print("Dec " + DECpass);
+
     return 0;
 }
